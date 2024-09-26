e5264abf8274af25e891c2a370fbd6b3ba6b3a32
Michael Brady <michaelbrady816@gmail.com>
2024-09-26 10:09:51 -0500

    worked on contact form

--- client/src/Components/Contact/Contact.tsx
+++ client/src/Components/Contact/Contact.tsx
@@ -1,4 +1,60 @@
+import './styles.css';
+
 const Contact = () => {
-	return <section>Contact</section>;
+	return (
+		<section id='contact' className='contact-container'>
+			<h1 className='contact-title'>Contact Us</h1>
+			<form
+				action='https://formsubmit.co/mbrady8008@gmail.com'
+				method='post'
+				className='contact-form'
+			>
+				<div className='form-item'>
+					<input
+						type='text'
+						name='name'
+						className='form-input'
+						placeholder='John/Jane Doe'
+						required
+					></input>
+					<label htmlFor='name' className='form-label'>
+						{' '}
+						Name{' '}
+					</label>
+				</div>
+				<div className='form-item'>
+					<input
+						type='email'
+						name='email'
+						className='form-input'
+						placeholder='youremail@email.com'
+						required
+					></input>
+					<label htmlFor='email' className='form-label'>
+						Email
+					</label>
+				</div>
+				<div className='form-item'>
+					<textarea
+						name='message'
+						className='form-input'
+						placeholder='Goals'
+						required
+					></textarea>
+					<label htmlFor='message' className='form-label'>
+						Message
+					</label>
+				</div>
+				<input
+					type='hidden'
+					name='_autoresponse'
+					value="Thank you for reaching out!  If you're not too busy, why not take time to read the blogs on the website?"
+				></input>
+				<button type='submit' className='submit-button'>
+					Submit
+				</button>
+			</form>
+		</section>
+	);
 };
 export default Contact;
